4b55ac14fca85723078025eaa6e8595b9ffaae4e
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-12-12 17:45:06 -0600

    connected to database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
+const connect = require('./config/database');
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get('/api/health', (req, res) => {
     res.send('I am alive!');
 });
+
+connect();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
